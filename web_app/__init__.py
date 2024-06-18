3f72d6d0da605147ff79479090b9342589a5f435
MikeF66 <165584598+MikeF66@users.noreply.github.com>
2024-06-18 23:07:53 +0300

    Update __init__.py

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -1,1 +1,10 @@
+from flask import Flask
 
+
+app = Flask(__name__)
+
+app.config['UPLOAD_FOLDER'] = 'web_app/static/uploads/'
+# web_app.config['SECRET_KEY'] = 'you_will_never_guess'
+
+
+from web_app import routes
