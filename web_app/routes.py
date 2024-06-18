e87f10ffcdd6c1698a725f29805930c12da80d56
MikeF66 <165584598+MikeF66@users.noreply.github.com>
2024-06-18 23:25:08 +0300

    Update routes.py

--- web_app/routes.py
+++ web_app/routes.py
@@ -3,30 +3,6 @@ import os
 from werkzeug.utils import secure_filename
 from web_app import app
 
-# posts = []
-# @web_app.route("/blog/", methods=["GET", "POST"])
-# def index():
-#     if request.method == "POST":
-#         title = request.form.get('title')
-#         image = request.files.get('image')
-#         content = request.form.get('content')
-#         if image:
-#             filename = secure_filename(image.filename)
-#             image_path = os.path.join(web_app.config['UPLOAD_FOLDER'], filename)
-#             image.save(image_path)
-#             image_path = os.path.join('uploads', filename).replace("\\", "/") # относительный путь для отображения
-#             print(image_path)
-#         else:
-#             image_path = None
-#         post = {
-#             'title': title,
-#             'image_path': image_path,
-#             'content': content
-#         }
-#         posts.append(post)
-#         return redirect(url_for('index'))
-#     return render_template('blog.html', posts=posts)
-
 cards = []
 @app.route("/", methods=["GET", "POST"])
 def profile():
@@ -63,22 +39,4 @@ def profile():
         cards.append(card)
         return redirect(url_for('profile'))
     return render_template('profile.html', cards=cards)
-
-
-
-
-
-
-# @web_app.route('/upload', methods=['POST'])
-# def upload_file():
-#     if 'file' not in request.files:
-#         return redirect(request.url)
-#     file = request.files['file']
-#     if file.filename == '':
-#         return redirect(request.url)
-#     if file:
-#         filename = secure_filename(file.filename)
-#         file.save(os.path.join(web_app.config['UPLOAD_FOLDER'], filename))
-#         # Здесь вы можете добавить пост в базу данных, включая путь к файлу
-#         return redirect(url_for('index'))
 
